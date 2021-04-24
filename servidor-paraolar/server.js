2d5496c1905378f8f58075ff8c262f88c0b6bc2a
Eny Martins <enyalvesmartins@gmail.com>
2021-04-24 17:38:01 -0300

    adicionando requisição por parâmetros 'query'

--- servidor-paraolar/server.js
+++ servidor-paraolar/server.js
@@ -7,8 +7,14 @@ app.get("/estados/cidades", (req, res) => {
   res.status(200).json(estados);
 });
 
+//filtrar sigla diretamente na url;
 app.get("/estados/:sigla", (req, res)=>{
   let siglaRequisitada = req.params.sigla;
+  res.status(200).json(estados.find(estado => estado.sigla == siglaRequisitada));
+})
+
+app.get("/estados/sigla", (req, res)=>{
+  let siglaRequisitada = req.query.sigla;
   res.status(200).json(estados.find(estado => estado.sigla == siglaRequisitada));
 })
 
