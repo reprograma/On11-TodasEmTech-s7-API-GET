63be11f6654f2ece3badd046dd587f3a0a4415a4
Priscila <pry_rocha@outlook.com>
2021-04-24 10:32:22 -0300

    Atualização

--- servidor-paraolar/server.js
+++ servidor-paraolar/server.js
@@ -5,18 +5,14 @@ const app = express()
 
 //Retorna lista completa de estados-cidades
 app.get("/estados/todos", (request, response) => {
-
     response.status(200).json(estadosCidades)
-
 })
 
 //Retorna nome, sigla e cidades de uma sigla especifica
 app.get("/estados/filtro", (request, response) => {
-
     const siglaRequisitada = request.query.siglas
     response.status(200).json(estadosCidades.find(estado => estado.sigla == siglaRequisitada))
 })
-
 app.listen(8080, () => {
     console.log("Teste porta 8080")
 })
