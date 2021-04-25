cb005a14278bb3327419cbea5ba3194e2df050aa
Beatriz Ramerindo dos Santos <janeramerindo@gmail.com>
2021-04-24 22:26:22 -0300

    Exercicios da semana realizado  GET

--- servidor-paraolar/server.js
+++ servidor-paraolar/server.js
@@ -6,13 +6,12 @@ const app = express();
 const port = 3032;
 
 
-
 app.get("/estados/todos", (request, response) => response.status(200).send(estados));
 
 app.get("/estados/:sigla", (request, response) => {
     const estado = estados.find(estado => estado.sigla == request.params.sigla);
     if (estado) response.status(200).send(estado);
-    else response.status(404);
+    else response.status(404).send({ 'erro' : 'sigla nao encontrada'});
 })
 
 app.listen(port, () => console.log(`rodando na porta ${port}`))
